45651e880bfad8790955ad98bfcde63dcf2144bb
kynail <kylian.gence@epitech.eu>
2022-05-30 10:57:52 +0200

    modif dashboardpage

--- lib/view/dashboard_page.dart
+++ lib/view/dashboard_page.dart
@@ -15,6 +15,8 @@ import '../../model/profile.dart';
 import '../controller/profil_controller.dart';
 import '../controller/store_controller.dart';
 import '../utils/translations.dart';
+import 'package:inwealth/view/projet_page.dart';
+
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -71,9 +73,9 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     List<String> _purposes = [
-      AppLocalizations.of(context)?.translate('local_realEstate', 0) ?? "Purchasing real estate",
-      AppLocalizations.of(context)?.translate('restructuring', 0) ?? "Restructuring the compagny",
-      AppLocalizations.of(context)?.translate('selling_biz', 0) ?? "Selling your business",
+      AppLocalizations.of(context)?.translate('local_realEstate', 0) ?? "Purchasing real estate.",
+      AppLocalizations.of(context)?.translate('restructuring', 0) ?? "Restructuring the compagny.",
+      AppLocalizations.of(context)?.translate('selling_biz', 0) ?? "Selling your business.",
       // AppLocalizations.of(context)?.translate('selling_biz', 0) ?? "humk ",
 
     ];
@@ -297,129 +299,132 @@ class Cardhome extends StatelessWidget {
       children: [
         Padding(
           padding: EdgeInsets.all(15),
-          child: FlipCard(
-            key: cardkey,
-            fill: Fill.fillBack,
-            flipOnTouch: false,
-            controller: controller,
-            direction: FlipDirection.HORIZONTAL,
-            front: Container(
-              child: Badge(
-                elevation: 10,
-                position: BadgePosition.topEnd(top: -9, end: -5),
-                badgeContent:
-                    Text('7', style: TextStyle(fontWeight: FontWeight.w500)),
-                badgeColor: Color(0xFF7F7A93),
-                child: Stack(
-                  fit: StackFit.passthrough,
-                  children: [
-                    Container(
-                      height: 150.0,
-                      width: 150.0,
-                      decoration: BoxDecoration(
-                        color: Color(0xFF5E5B74),
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        image: DecorationImage(
-                            colorFilter: ColorFilter.mode(
-                                Color(0xFFD5C6AC).withOpacity(1.0),
-                                BlendMode.srcATop),
-                            image: AssetImage('assets/test.png'),
-                            fit: BoxFit.cover),
-                        shape: BoxShape.rectangle,
+          child: GestureDetector(
+            onTap: () => Get.to(ProjetPage()),
+            child: FlipCard(
+              key: cardkey,
+              fill: Fill.fillBack,
+              flipOnTouch: false,
+              // controller: controller,
+              direction: FlipDirection.HORIZONTAL,
+              front: Container(
+                child: Badge(
+                  elevation: 10,
+                  position: BadgePosition.topEnd(top: -9, end: -5),
+                  badgeContent:
+                      Text('7', style: TextStyle(fontWeight: FontWeight.w500)),
+                  badgeColor: Color(0xFF7F7A93),
+                  child: Stack(
+                    fit: StackFit.passthrough,
+                    children: [
+                      Container(
+                        height: 150.0,
+                        width: 150.0,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF5E5B74),
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                  Color(0xFFD5C6AC).withOpacity(1.0),
+                                  BlendMode.srcATop),
+                              image: AssetImage('assets/test.png'),
+                              fit: BoxFit.cover),
+                          shape: BoxShape.rectangle,
+                        ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Container(
-                        height: 150,
-                        width: 150,
-                        child: Center(
-                          // child: Obx( () =>
-                          child: Text(
-                            project,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.white),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          height: 150,
+                          width: 150,
+                          child: Center(
+                            // child: Obx( () =>
+                            child: Text(
+                              project,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            // ),
                           ),
-                          // ),
                         ),
                       ),
-                    ),
-                    Positioned(
-                      bottom: -8,
-                      right: -5,
-                      child: TextButton(
-                        onPressed: () => cardkey.currentState?.toggleCard(),
-                        style: ButtonStyle(),
-                        // padding: EdgeInsets.all(10.0),
-                        child: Column(
-                          children: [
-                            Icon(Icons.info_rounded, color: Colors.white)
-                          ],
+                      Positioned(
+                        bottom: -8,
+                        right: -5,
+                        child: TextButton(
+                          onPressed: () => cardkey.currentState?.toggleCard(),
+                          style: ButtonStyle(),
+                          // padding: EdgeInsets.all(10.0),
+                          child: Column(
+                            children: [
+                              Icon(Icons.info_rounded, color: Colors.white)
+                            ],
+                          ),
                         ),
-                      ),
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-            back: Container(
-              child: Badge(
-                position: BadgePosition.topEnd(top: -9, end: -5),
-                badgeContent: Text('7'),
-                badgeColor: Color(0xFF5E5B74),
-                child: Stack(
-                  children: [
-                    Container(
-                      height: 150.0,
-                      width: 150.0,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(16)),
-                        image: DecorationImage(
-                            colorFilter: ColorFilter.mode(
-                                Color(0xFFD5C6AC).withOpacity(1.0),
-                                BlendMode.srcATop),
-                            image: AssetImage('assets/test.png'),
-                            fit: BoxFit.cover),
-                        shape: BoxShape.rectangle,
+              back: Container(
+                child: Badge(
+                  position: BadgePosition.topEnd(top: -9, end: -5),
+                  badgeContent: Text('7'),
+                  badgeColor: Color(0xFF5E5B74),
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: 150.0,
+                        width: 150.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(16)),
+                          image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                  Color(0xFFD5C6AC).withOpacity(1.0),
+                                  BlendMode.srcATop),
+                              image: AssetImage('assets/test.png'),
+                              fit: BoxFit.cover),
+                          shape: BoxShape.rectangle,
+                        ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Container(
-                        height: 150,
-                        width: 150,
-                        child: Center(
-                          child: Text(
-                            "BACK",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.white),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          height: 150,
+                          width: 150,
+                          child: Center(
+                            child: Text(
+                              "BACK",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Positioned(
-                      bottom: -8,
-                      right: -5,
-                      child: TextButton(
-                        onPressed: () => cardkey.currentState?.toggleCard(),
-                        style: ButtonStyle(),
-                        // padding: EdgeInsets.all(10.0),
-                        child: Column(
-                          children: [
-                            Icon(Icons.info_rounded, color: Colors.white)
-                          ],
+                      Positioned(
+                        bottom: -8,
+                        right: -5,
+                        child: TextButton(
+                          onPressed: () => cardkey.currentState?.toggleCard(),
+                          style: ButtonStyle(),
+                          // padding: EdgeInsets.all(10.0),
+                          child: Column(
+                            children: [
+                              Icon(Icons.info_rounded, color: Colors.white)
+                            ],
+                          ),
                         ),
-                      ),
-                    )
-                    // Align(
-                    //   alignment: Alignment.topRight,
-                    //   child: Badge(
-                    //     badgeContent: Text('7'),
-                    //     position: BadgePosition.topEnd(),
-                    //     badgeColor: Color(0xFF5E5B74),
-                    //   ),
-                    // ),
-                  ],
+                      )
+                      // Align(
+                      //   alignment: Alignment.topRight,
+                      //   child: Badge(
+                      //     badgeContent: Text('7'),
+                      //     position: BadgePosition.topEnd(),
+                      //     badgeColor: Color(0xFF5E5B74),
+                      //   ),
+                      // ),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -444,101 +449,104 @@ class Card2home extends StatelessWidget {
       children: [
         Padding(
           padding: EdgeInsets.all(15),
-          child: FlipCard(
-            fill: Fill.fillBack,
-            direction: FlipDirection.HORIZONTAL,
-            //flipOnTouch: false,
-            front: Container(
-              child: Badge(
-                position: BadgePosition.topEnd(top: -9, end: -5),
-                badgeContent: Text('6'),
-                badgeColor: Color(0xFF5E5B74),
-                child: Stack(
-                  fit: StackFit.passthrough,
-                  children: [
-                    Container(
-                      height: 190.0,
-                      width: 250.0,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(16)),
-                        image: DecorationImage(
-                            colorFilter: ColorFilter.mode(
-                                Color(0xFF7F7A93).withOpacity(1.0),
-                                BlendMode.srcATop),
-                            image: AssetImage('assets/test.png'),
-                            fit: BoxFit.cover),
-                        shape: BoxShape.rectangle,
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Container(
-                        height: 190,
-                        width: 250,
-                        child: Center(
-                          child: Text(
-                            "FRONT",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: -6,
-                      right: -5,
-                      child: TextButton(
-                        onPressed: () => controller.toggleCard(),
-                        style: ButtonStyle(),
-                        // padding: EdgeInsets.all(10.0),
-                        child: Column(
-                          children: [
-                            Icon(Icons.info_rounded, color: Colors.white)
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            back: Container(
-              child: Badge(
-                position: BadgePosition.topEnd(top: -9, end: -5),
-                badgeContent: Text('6'),
-                badgeColor: Color(0xFF5E5B74),
-                child: Stack(
-                  //fit: StackFit.passthrough,
-                  children: [
-                    Container(
-                      height: 190.0,
-                      width: 250.0,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        image: DecorationImage(
-                            colorFilter: ColorFilter.mode(
-                                Colors.black.withOpacity(0.2),
-                                BlendMode.srcATop),
-                            image: AssetImage('assets/test.png'),
-                            fit: BoxFit.cover),
-                        shape: BoxShape.rectangle,
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Container(
+          child: GestureDetector(
+            onTap: () => Get.to(DashboardPage()),
+            child: FlipCard(
+              fill: Fill.fillBack,
+              direction: FlipDirection.HORIZONTAL,
+              //flipOnTouch: false,
+              front: Container(
+                child: Badge(
+                  position: BadgePosition.topEnd(top: -9, end: -5),
+                  badgeContent: Text('6'),
+                  badgeColor: Color(0xFF5E5B74),
+                  child: Stack(
+                    fit: StackFit.passthrough,
+                    children: [
+                      Container(
                         height: 190.0,
                         width: 250.0,
-                        child: Center(
-                          child: Text(
-                            "BACK",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.white),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(16)),
+                          image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                  Color(0xFF7F7A93).withOpacity(1.0),
+                                  BlendMode.srcATop),
+                              image: AssetImage('assets/test.png'),
+                              fit: BoxFit.cover),
+                          shape: BoxShape.rectangle,
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          height: 190,
+                          width: 250,
+                          child: Center(
+                            child: Text(
+                              "FRONT",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                      Positioned(
+                        bottom: -6,
+                        right: -5,
+                        child: TextButton(
+                          onPressed: () => controller.toggleCard(),
+                          style: ButtonStyle(),
+                          // padding: EdgeInsets.all(10.0),
+                          child: Column(
+                            children: [
+                              Icon(Icons.info_rounded, color: Colors.white)
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              back: Container(
+                child: Badge(
+                  position: BadgePosition.topEnd(top: -9, end: -5),
+                  badgeContent: Text('6'),
+                  badgeColor: Color(0xFF5E5B74),
+                  child: Stack(
+                    //fit: StackFit.passthrough,
+                    children: [
+                      Container(
+                        height: 190.0,
+                        width: 250.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          image: DecorationImage(
+                              colorFilter: ColorFilter.mode(
+                                  Colors.black.withOpacity(0.2),
+                                  BlendMode.srcATop),
+                              image: AssetImage('assets/test.png'),
+                              fit: BoxFit.cover),
+                          shape: BoxShape.rectangle,
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          height: 190.0,
+                          width: 250.0,
+                          child: Center(
+                            child: Text(
+                              "BACK",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
---
+++ lib/view/projet_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProjetPage extends StatefulWidget {
+  ProjetPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProjetPage> createState() => _ProjetPageState();
+}
+
+class _ProjetPageState extends State<ProjetPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
